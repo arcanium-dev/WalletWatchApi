dfd0637483502a1e432efaec109f52e074e80a24
abstrxtt <bmtaki335@gmail.com>
2023-02-20 21:04:05 +0900

    Setup configuration for server Add plugin files

--- src/main/kotlin/com/arcanium/Application.kt
+++ src/main/kotlin/com/arcanium/Application.kt
@@ -5,9 +5,8 @@ import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import com.arcanium.plugins.*
 
-fun main() {
-    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
-        .start(wait = true)
+fun main(args: Array<String>) {
+    EngineMain.main(args)
 }
 
 fun Application.module() {
---
+++ src/main/kotlin/com/arcanium/plugins/Security.kt
@@ -0,0 +1,30 @@
+package com.arcanium.plugins
+
+import com.auth0.jwt.JWT
+import com.auth0.jwt.algorithms.Algorithm
+import io.ktor.server.application.*
+import io.ktor.server.auth.*
+import io.ktor.server.auth.jwt.*
+
+fun Application.configureSecurity() {
+    authentication {
+        jwt {
+            val environment = this@configureSecurity.environment
+            val jwtAudience = environment.config.property("jwt.audience").getString()
+            realm = this@configureSecurity.environment.config.property("jwt.realm").getString()
+            verifier(
+                JWT
+                    .require(Algorithm.HMAC256("secret"))
+                    .withAudience(jwtAudience)
+                    .withIssuer(jwtAudience)
+                    .build()
+            )
+            validate { credential ->
+                if (credential.payload.audience.contains(jwtAudience)) {
+                    JWTPrincipal(credential.payload)
+                } else null
+            }
+        }
+    }
+
+}
