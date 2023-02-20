09f253a62e96837a64b21ee52d9b57803e80b77c
abstrxtt <bmtaki335@gmail.com>
2023-02-20 21:34:01 +0900

    Integrate MongoDb and connect to Backend

--- src/main/kotlin/com/arcanium/Application.kt
+++ src/main/kotlin/com/arcanium/Application.kt
@@ -1,14 +1,29 @@
 package com.arcanium
 
+import com.arcanium.plugins.configureMonitoring
+import com.arcanium.plugins.configureRouting
+import com.arcanium.plugins.configureSecurity
+import com.arcanium.plugins.configureSerialization
 import io.ktor.server.application.*
-import io.ktor.server.engine.*
 import io.ktor.server.netty.*
-import com.arcanium.plugins.*
+import org.litote.kmongo.coroutine.coroutine
+import org.litote.kmongo.reactivestreams.KMongo
 
 fun main(args: Array<String>) {
     EngineMain.main(args)
 }
 
 fun Application.module() {
+    val mongoPassword = System.getenv("MONGO_PW")
+    val dbName = "wallet-watch-01"
+    val db = KMongo.createClient(
+        connectionString = "mongodb+srv://<username>:$mongoPassword@cluster0.pqknvkf.mongodb.net/$dbName?retryWrites=true&w=majority"
+    )
+        .coroutine
+        .getDatabase(dbName)
+
     configureRouting()
+    configureSerialization()
+    configureMonitoring()
+    configureSecurity()
 }
